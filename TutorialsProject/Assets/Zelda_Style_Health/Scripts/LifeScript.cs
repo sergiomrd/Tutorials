b5f2791c1621fe324631ea5bc862a60d43b15b74
Sergio Malonda <sergio.del93@gmail.com>
2015-12-18 22:56:36 +0100

    First commit

---
+++ TutorialsProject/Assets/Zelda_Style_Health/Scripts/LifeScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LifeScript : MonoBehaviour {
+
+	public GameObject heart;
+	public int Max_Health;
+	public int Remaining_Health;
+
+	private Transform lastPosition;
+	private GameObject newHeart;
+
+	public void AddHeart()
+	{
+		
+	}
+
+
+}
